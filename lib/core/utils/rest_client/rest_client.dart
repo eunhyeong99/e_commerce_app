aab97b286cabc1855f0a0ecb98a3818e5a326b9d
jo-eunhyeong <whdmsgud8@naver.com>
2025-04-17 17:55:40 +0900

    feat: mock datat 전송 Test

--- lib/core/utils/rest_client/rest_client.dart
+++ lib/core/utils/rest_client/rest_client.dart
@@ -1,13 +1,13 @@
-// import 'package:dio/dio.dart';
-//
-// class RestClient {
-//   final Dio _dio = Dio();
-//
-//   static final RestClient _instance = RestClient._internal();
-//
-//   Dio get getDio => _dio;
-//
-//   factory RestClient() => _instance;
-//
-//   RestClient._internal();
-// }
+import 'package:dio/dio.dart';
+
+class RestClient {
+  final Dio _dio = Dio();
+
+  static final RestClient _instance = RestClient._internal();
+
+  Dio get getDio => _dio;
+
+  factory RestClient() => _instance;
+
+  RestClient._internal();
+}
---
+++ lib/data/data_source/remote/display.api.g.dart
@@ -0,0 +1,83 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'display.api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+
+class _DisplayApi implements DisplayApi {
+  _DisplayApi(this._dio, {this.baseUrl});
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+
+  @override
+  Future<ResponseWrapper<List<MenuDto>>> getMenusByMallType(
+    String mallType,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ResponseWrapper<List<MenuDto>>>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/menus/${mallType}',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ResponseWrapper<List<MenuDto>> _value;
+    try {
+      _value = ResponseWrapper<List<MenuDto>>.fromJson(
+        _result.data!,
+        (json) =>
+            json is List<dynamic>
+                ? json
+                    .map<MenuDto>(
+                      (i) => MenuDto.fromJson(i as Map<String, dynamic>),
+                    )
+                    .toList()
+                : List.empty(),
+      );
+    } on Object catch (e, s) {
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(String dioBaseUrl, String? baseUrl) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
